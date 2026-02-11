a3d6b71b1a5770c7da2ac0678cf070d4787c714f
Eckhoee <8399460+Eckhoee@users.noreply.github.com>
2026-02-10 21:40:09 -0500

    Added new features to engineer

--- src/data_pipelines.py
+++ src/data_pipelines.py
@@ -61,7 +61,10 @@ feature_eng = ColumnTransformer([
     ["nums__BsmtFullBath", "nums__BsmtHalfBath", "nums__TotalBsmtSF"]),
     ("AbvGrBathPerSF", make_pipeline(sum_ratio_pipeline()), ["nums__FullBath", "nums__HalfBath", "nums__GrLivArea"]),
     ("AbvGrSFPerRoom", make_pipeline(sum_ratio_pipeline()), ["nums__1stFlrSF", "nums__2ndFlrSF", "nums__TotRmsAvgGrd"]),
-    ("BmstQual_Cond", make_pipeline(ratio_pipeline()), ["na_ord_cols__BmstQual", "na_ord_cols__BsmtCond"])],
+    ("BmstQual_Cond", make_pipeline(ratio_pipeline()), ["na_ord_cols__BmstQual", "na_ord_cols__BsmtCond"]),
+    ("GarageQual_Cond", make_pipeline(ratio_pipeline()), ["na_ord_cols__GarageQual", "na_ord_cols__GarageCond"]),
+    ("GarageFinish_Qual_Cond", make_pipeline(sum_ratio_pipeline()), ["na_ord_cols__GarageQual", "na_ord_cols__GarageCond", "na_ord_cols__GarageFinish"])
+    ],
     remainder="passthrough"
 )
 
